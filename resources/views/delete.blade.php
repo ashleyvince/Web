a4c9f949a61e91f4d78680ee871d344735c532ec
unknown <baba.ashleyvince@gmail.com>
2022-06-29 11:29:52 +0800

    Final Web Project

---
+++ resources/views/delete.blade.php
@@ -0,0 +1,14 @@
+<?php
+$con = mysqli_connect("localhost", "root", "");
+	
+$db = mysqli_select_db($con,"newbies_db"); 
+$query = mysqli_query($con,"DELETE FROM tbl_students WHERE Student_ID = '".$_GET['id']."'") or die(mysql_error());
+// $query = mysqli_query($con,"Update registration SET status='Deleted' WHERE studID ='".$_GET['id']."'") or die(mysqli_error());
+if(!$query){
+	echo "Record not deleted!";
+}
+else{
+	echo"Record successfully deleted";
+}
+echo "<a href='http://127.0.0.1:8000/home'><input type='button' value='Back'/></a>";
+?>
